d161c3c0cdc710be5a877c0c749b82d38b12f636
Richard <richard@ekohe.com>
2009-11-03 22:37:11 +0800

    compare current_user when current_user is a subject and compare unless block

--- lib/rails_best_practices/checks/use_scope_access_check.rb
+++ lib/rails_best_practices/checks/use_scope_access_check.rb
@@ -2,11 +2,13 @@ require 'rails_best_practices/checks/check'
 
 module RailsBestPractices
   module Checks
-    # Check a controller to make sure use scope to 
+    # Check a controller to make sure use scope access
+    #
+    # Implementation: simply check if or unless compare with current_user or current_user.id and there is a redirect_to message in if or unless block
     class UseScopeAccessCheck < Check
       
       def interesting_nodes
-        [:if]
+        [:if, :unless]
       end
       
       def interesting_files
@@ -21,7 +23,10 @@ module RailsBestPractices
       
       def current_user_redirect?(node)
         condition_node = node.call
-        condition_node.message == :== and current_user?(condition_node.arguments.call) and node.false_node.method_body.any? {|n| n.message == :redirect_to}
+        
+        condition_node.message == :== and 
+        (current_user?(condition_node.arguments.call) or current_user?(condition_node.subject)) and 
+        (node.false_node.method_body.any? {|n| n.message == :redirect_to} or node.true_node.method_body.any? {|n| n.message == :redirect_to})
       end
       
       def current_user?(call_node)
--- spec/rails_best_practices/checks/use_scope_access_check_spec.rb
+++ spec/rails_best_practices/checks/use_scope_access_check_spec.rb
@@ -5,43 +5,167 @@ describe RailsBestPractices::Checks::UseScopeAccessCheck do
     @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::UseScopeAccessCheck.new)
   end
   
-  it "shoud use scope access" do
-    content = <<-EOF
-    class PostsController < ApplicationController
+  context "if" do
+    it "shoud use scope access" do
+      content = <<-EOF
+      class PostsController < ApplicationController
       
-      def edit
-        @post = Post.find(params[:id])
+        def edit
+          @post = Post.find(params[:id])
         
-        if @post.user != current_user
-          flash[:warning] = 'Access Denied'
-          redirect_to posts_url
+          if @post.user != current_user
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
         end
       end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
     end
-    EOF
-    @runner.check('app/controller/posts_controller.rb', content)
-    errors = @runner.errors
-    errors.should_not be_empty
-    errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
+
+    it "shoud use scope access by comparing with id" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          if @post.user_id != current_user.id
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
+    end
+
+    it "shoud use scope access with current_user ==" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          if current_user != @post.user
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
+    end
+
+    it "shoud use scope access by current_user.id ==" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          if current_user.id != @post.user_id
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
+    end
   end
 
-  it "shoud use scope access by comparing with id" do
-    content = <<-EOF
-    class PostsController < ApplicationController
+  context "unless" do
+    it "shoud use scope access" do
+      content = <<-EOF
+      class PostsController < ApplicationController
 
-      def edit
-        @post = Post.find(params[:id])
+        def edit
+          @post = Post.find(params[:id])
 
-        if @post.user_id != current_user.id
-          flash[:warning] = 'Access Denied'
-          redirect_to posts_url
+          unless @post.user == current_user
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
         end
       end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:6 - use scope access"
     end
-    EOF
-    @runner.check('app/controller/posts_controller.rb', content)
-    errors = @runner.errors
-    errors.should_not be_empty
-    errors[0].to_s.should == "app/controller/posts_controller.rb:7 - use scope access"
+
+    it "shoud use scope access by comparing with id" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          unless @post.user_id == current_user.id
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:6 - use scope access"
+    end
+
+    it "shoud use scope access with current_user ==" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          unless current_user == @post.user
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:6 - use scope access"
+    end
+
+    it "shoud use scope access by current_user.id ==" do
+      content = <<-EOF
+      class PostsController < ApplicationController
+
+        def edit
+          @post = Post.find(params[:id])
+
+          unless current_user.id == @post.user_id
+            flash[:warning] = 'Access Denied'
+            redirect_to posts_url
+          end
+        end
+      end
+      EOF
+      @runner.check('app/controller/posts_controller.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/controller/posts_controller.rb:6 - use scope access"
+    end
   end
 end
