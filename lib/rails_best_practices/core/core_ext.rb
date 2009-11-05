0e5fdcca4e248cbc71ef28750b39a3e263f8f3aa
Richard <richard@ekohe.com>
2009-11-05 11:11:07 +0800

    hack NilClass, return nil after NilClass receiving any method not defined

--- lib/rails_best_practices/core/core_ext.rb
+++ lib/rails_best_practices/core/core_ext.rb
@@ -3,3 +3,9 @@ module Enumerable
     inject({}) {|h,v| h[v]=h[v].to_i+1; h}.reject{|k,v| v==1}.keys
   end
 end
+
+class NilClass
+  def method_missing(method_sym, *arguments, &block)
+    return nil
+  end
+end
