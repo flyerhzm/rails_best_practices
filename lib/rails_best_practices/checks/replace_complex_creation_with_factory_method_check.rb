24068396523a729a3f6027e87678b206f176acad
Richard <richard@ekohe.com>
2009-11-05 15:21:29 +0800

    support replace_complex_creation_with_factory_method

---
+++ lib/rails_best_practices/checks/replace_complex_creation_with_factory_method_check.rb
@@ -0,0 +1,51 @@
+require 'rails_best_practices/checks/check'
+
+module RailsBestPractices
+  module Checks
+    # Check a controller file to make sure that complex model creation should not exist in controller, move it to model factory method
+    #
+    # Implementation: check the count of variable attribute assignment calling before saving, if large than defined attribute assignment count, then it's a complex creation, 
+    class ReplaceComplexCreationWithFactoryMethodCheck < Check
+      
+      def interesting_nodes
+        [:defn]
+      end
+      
+      def interesting_files
+        /_controller.rb$/
+      end
+      
+      def initialize(options = {})
+        super()
+        @attrasgn_count = options[:attribute_assignment_count] || 2
+      end
+      
+      def evaluate_start(node)
+        @variables = {}
+        node.recursive_children do |child|
+          case child.node_type
+          when :attrasgn
+            attribute_assignment(child)
+          when :call
+            call_assignment(child)
+          else
+          end
+        end
+        @variables = nil
+      end
+      
+      def attribute_assignment(node)
+        variable = node.subject
+        @variables[variable] ||= 0
+        @variables[variable] += 1
+      end
+      
+      def call_assignment(node)
+        if node.message == :save
+          variable = node.subject
+          add_error "replace complex creation with factory method" if @variables[variable] > @attrasgn_count
+        end
+      end
+    end
+  end
+end
---
+++ spec/rails_best_practices/checks/replace_complex_creation_with_factory_method_check_spec.rb
@@ -0,0 +1,76 @@
+require File.join(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe RailsBestPractices::Checks::AddModelVirtualAttributeCheck do
+  before(:each) do
+    @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::ReplaceComplexCreationWithFactoryMethodCheck.new)
+  end
+  
+  it "should replace complex creation with factory method" do
+    content = <<-EOF
+    class InvoiceController < ApplicationController
+      
+      def create
+        @invoice = Invoice.new(params[:invoice])
+        @invoice.address = current_user.address
+        @invoice.phone = current_user.phone
+        @invoice.vip = (@invoice.amount > 1000)
+        
+        if Time.now.day > 15
+          @invoice.deliver_time = Time.now + 2.month
+        else
+          @invoice.deliver_time = Time.now + 1.month
+        end
+        
+        @invoice.save
+      end
+    end
+    EOF
+    @runner.check('app/controller/invoices_controller.rb', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors[0].to_s.should == "app/controller/invoices_controller.rb:3 - replace complex creation with factory method"
+  end
+
+  it "should not replace complex creation with factory method with simple creation" do
+    content = <<-EOF
+    class InvoiceController < ApplicationController
+
+      def create
+        @invoice = Invoice.new(params[:invoice])
+        @invoice.address = current_user.address
+        @invoice.phone = current_user.phone
+        @invoice.save
+      end
+    end
+    EOF
+    @runner.check('app/controller/invoices_controller.rb', content)
+    errors = @runner.errors
+    errors.should be_empty
+  end
+
+  it "should not replace complex creation with factory method when attrasgn_count is 5" do
+    content = <<-EOF
+    class InvoiceController < ApplicationController
+
+      def create
+        @invoice = Invoice.new(params[:invoice])
+        @invoice.address = current_user.address
+        @invoice.phone = current_user.phone
+        @invoice.vip = (@invoice.amount > 1000)
+
+        if Time.now.day > 15
+          @invoice.deliver_time = Time.now + 2.month
+        else
+          @invoice.deliver_time = Time.now + 1.month
+        end
+
+        @invoice.save
+      end
+    end
+    EOF
+    @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::ReplaceComplexCreationWithFactoryMethodCheck.new(:attribute_assignment_count => 5))
+    @runner.check('app/controller/invoices_controller.rb', content)
+    errors = @runner.errors
+    errors.should be_empty
+  end
+end
