acb48f153dfe000df2b7b66c59d45e8a51f0bc6d
Richard Huang <flyerhzm@gmail.com>
2009-11-07 22:09:05 +0800

    copy underscore method from rails

--- lib/rails_best_practices/core/core_ext.rb
+++ lib/rails_best_practices/core/core_ext.rb
@@ -4,6 +4,17 @@ module Enumerable
   end
 end
 
+class String
+  # copy from rails
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
+
 class NilClass
   def method_missing(method_sym, *arguments, &block)
     return nil
