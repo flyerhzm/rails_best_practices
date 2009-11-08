1c4d5a222dcb23b80c5512b79c109267bfd71262
Richard Huang <flyerhzm@gmail.com>
2009-11-08 15:46:55 +0800

    support isolate_seed_data check

---
+++ lib/rails_best_practices/checks/isolate_seed_data_check.rb
@@ -0,0 +1,46 @@
+require 'rails_best_practices/checks/check'
+
+module RailsBestPractices
+  module Checks
+    # Check a migration file to make sure not to insert data in migration, move them to seed file.
+    #
+    # Implementation: check if there are :create, :create!, and :new with :save or :save! exist, the migration file needs isolate seed data.
+    class IsolateSeedDataCheck < Check
+
+      def interesting_nodes
+        [:call, :lasgn]
+      end
+
+      def interesting_files
+        /db\/migrate\/.*rb/
+      end
+
+      def initialize
+        super
+        @new_variables = []
+      end
+
+      def evaluate_start(node)
+        if [:create, :create!].include? node.message
+          add_error("isolate seed data")
+        elsif :lasgn == node.node_type
+          remember_new_variable(node)
+        elsif [:save, :save!].include? node.message
+          add_error("isolate seed data") if new_record?(node)
+        end
+      end
+
+      private
+
+      def remember_new_variable(node)
+        unless node.grep_nodes({:node_type => :call, :message => :new}).empty?
+          @new_variables << node.subject.to_s
+        end
+      end
+
+      def new_record?(node)
+        @new_variables.include? node.subject.to_ruby
+      end
+    end
+  end
+end
---
+++ spec/rails_best_practices/checks/isolate_seed_data_check_spec.rb
@@ -0,0 +1,75 @@
+require File.join(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe RailsBestPractices::Checks::IsolateSeedDataCheck do
+  before(:each) do
+    @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::IsolateSeedDataCheck.new)
+  end
+
+  it "should isolate seed data" do
+    content = <<-EOF
+    class CreateRoles < ActiveRecord::Migration
+      def self.up
+        create_table "roles", :force => true do |t|
+          t.string :name
+        end
+
+        ["admin", "author", "editor", "account"].each do |name|
+          Role.create!(:name => name)
+        end
+      end
+
+      def self.down
+        drop_table "roles"
+      end
+    end
+    EOF
+    @runner.check('db/migrate/20090818130258_create_roles.rb', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors[0].to_s.should == "db/migrate/20090818130258_create_roles.rb:8 - isolate seed data"
+  end
+
+  it "should isolate seed data with new and save" do
+    content = <<-EOF
+    class CreateRoles < ActiveRecord::Migration
+      def self.up
+        create_table "roles", :force => true do |t|
+          t.string :name
+        end
+
+        ["admin", "author", "editor", "account"].each do |name|
+          role = Role.new(:name => name)
+          role.save!
+        end
+      end
+
+      def self.down
+        drop_table "roles"
+      end
+    end
+    EOF
+    @runner.check('db/migrate/20090818130258_create_roles.rb', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors[0].to_s.should == "db/migrate/20090818130258_create_roles.rb:9 - isolate seed data"
+  end
+
+  it "should not isolate seed data without data insert" do
+    content = <<-EOF
+    class CreateRoles < ActiveRecord::Migration
+      def self.up
+        create_table "roles", :force => true do |t|
+          t.string :name
+        end
+      end
+
+      def self.down
+        drop_table "roles"
+      end
+    end
+    EOF
+    @runner.check('db/migrate/20090818130258_create_roles.rb', content)
+    errors = @runner.errors
+    errors.should be_empty
+  end
+end
