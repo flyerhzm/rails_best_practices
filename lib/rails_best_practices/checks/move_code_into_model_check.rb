a4b76dd0f71c095873c09ab1cfc2372a5342cfcc
Richard Huang <flyerhzm@gmail.com>
2009-12-14 23:06:40 +0800

    add move_code_into_model_check support

---
+++ lib/rails_best_practices/checks/move_code_into_model_check.rb
@@ -0,0 +1,53 @@
+require 'rails_best_practices/checks/check'
+
+module RailsBestPractices
+  module Checks
+    # Check a view file to make sure there is no complex logic call for model.
+    #
+    # Implementation: Check if a local variable or instance variable called more than 3 times in if statuement, then it should more code into model.
+    class MoveCodeIntoModelCheck < Check
+
+      def interesting_nodes
+        [:if]
+      end
+
+      def interesting_files
+        VIEW_FILES
+      end
+
+      def evaluate_start(node)
+        @variables = {}
+        node.grep_nodes(:node_type => :call).each { |call_node| remember_call(call_node) }
+        check_errors
+      end
+
+      private
+
+      def check_errors
+        @variables.each do |node, count|
+          add_error "move code into model (#{node.to_ruby})" if count > 2
+        end
+      end
+
+      def remember_call(call_node)
+        variable_node = variable(call_node)
+        if variable_node
+          @variables[variable_node] ||= 0
+          @variables[variable_node] += 1
+        end
+      end
+
+      def variable(call_node)
+        while call_node.subject.node_type == :call
+          call_node = call_node.subject
+        end
+        subject_node = call_node.subject
+        if [:ivar, :lvar].include?(subject_node.node_type) and subject_node[1] != :_erbout
+          subject_node
+        else
+          nil
+        end
+      end
+    end
+  end
+end
---
+++ spec/rails_best_practices/checks/move_code_into_model_check_spec.rb
@@ -0,0 +1,41 @@
+require File.join(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe RailsBestPractices::Checks::MoveCodeIntoModelCheck do
+  before(:each) do
+    @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::MoveCodeIntoModelCheck.new)
+  end
+
+  it "should move code into model" do
+    content =<<-EOF
+    <% if current_user && (current_user == @post.user || @post.editors.include?(current_user)) %>
+      <%= link_to 'Edit this post', edit_post_url(@post) %>
+    <% end %>
+    EOF
+    @runner.check('app/views/posts/show.html.erb', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors[0].to_s.should == "app/views/posts/show.html.erb:1 - move code into model (@post)"
+  end
+
+  it "should move code into model with haml" do
+    content =<<-EOF
+- if current_user && (current_user == @post.user || @post.editors.include?(current_user))
+  = link_to 'Edit this post', edit_post_url(@post)
+    EOF
+    @runner.check('app/views/posts/show.html.haml', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors[0].to_s.should == "app/views/posts/show.html.haml:1 - move code into model (@post)"
+  end
+
+  it "should not move code into model" do
+    content =<<-EOF
+    <% if @post.editable_by?(current_user)) %>
+      <%= link_to 'Edit this post', edit_post_url(@post) %>
+    <% end %>
+    EOF
+    @runner.check('app/views/posts/show.html.erb', content)
+    errors = @runner.errors
+    errors.should be_empty
+  end
+end
