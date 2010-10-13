954a854edf0ed9af17028b5b74d97b867c032b17
Richard Huang <flyerhzm@gmail.com>
2010-10-13 23:56:22 +0800

    fix error for use say with time in migrations check

--- spec/rails_best_practices/checks/use_say_with_time_in_migrations_check_spec.rb
+++ spec/rails_best_practices/checks/use_say_with_time_in_migrations_check_spec.rb
@@ -52,4 +52,41 @@ describe RailsBestPractices::Checks::UseSayWithTimeInMigrationsCheck do
     errors = @runner.errors
     errors.should be_empty
   end
+
+  it "should not raise an error" do
+    content =<<-EOF
+    class AddAdmin < ActiveRecord::Migration
+
+      class Person < ActiveRecord::Base  
+      end
+      
+      class Blog < ActiveRecord::Base
+      end
+
+      def self.up
+        add_column :people, :admin, :boolean, :default => false, :null => false
+        add_column :people, :deactivated, :boolean, 
+                            :default => false, :null => false
+        
+        key = Crypto::Key.from_file("\#{RAILS_ROOT}/rsa_key.pub")
+        person = Person.new(:email => "admin@example.com",
+                            :name => "admin",
+                            :crypted_password => key.encrypt("admin"),
+                            :description => "")
+        person.admin = true
+        person.save!
+        Blog.create(:person_id => person.id)
+      end
+
+      def self.down
+        remove_column :people, :deactivated
+        Person.delete(Person.find_by_name("admin"))
+        remove_column :people, :admin
+      end
+    end
+    EOF
+    @runner.check('db/migrate/20101010080658_create_users.rb', content)
+    errors = @runner.errors
+    errors.should be_empty
+  end
 end
