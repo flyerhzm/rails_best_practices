a9511cb40098f4bbaaa8f776cf837b060a595a1d
Richard Huang <flyerhzm@gmail.com>
2010-11-30 23:32:53 +0800

    add i18n gem dependency and remove 1.9.1 test

--- rubies_test.sh
+++ rubies_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rvm ruby-1.8.7@rails_best_practices ruby -S bundle install      || exit $?
-rvm ruby-1.9.1@rails_best_practices ruby -S bundle install      || exit $?
 rvm ruby-1.9.2@rails_best_practices ruby -S bundle install      || exit $?
 rvm ree@rails_best_practices ruby -S bundle install             || exit $?
-rvm 1.8.7@rails_best_practices,1.9.1,1.9.2,ree rake spec:progress
+rvm 1.8.7@rails_best_practices
+rake spec:progress
+rvm 1.9.2@rails_best_practices
+rake spec:progress
+rvm ree@rails_best_practices
+rake spec:progress
