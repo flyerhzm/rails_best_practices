2d71758dbc61874df51f45180ed69c02a38f71c8
Richard Huang <flyerhzm@gmail.com>
2010-12-05 16:01:18 +0800

    check const node type

--- lib/rails_best_practices/core/visitable_sexp.rb
+++ lib/rails_best_practices/core/visitable_sexp.rb
@@ -14,23 +14,23 @@ class Sexp
   def children
     find_all { | sexp | Sexp === sexp }
   end
-  
+
   def is_language_node?
     first.class == Symbol
   end
-  
+
   def visitable_children
     parent = is_language_node? ? sexp_body : self
     parent.children
   end
-  
+
   def recursive_children(&handler)
     visitable_children.each do |child|
       handler.call child
       child.recursive_children(&handler)
     end
   end
-  
+
   def grep_nodes(options)
     return self if options.empty?
     node_type = options[:node_type]
@@ -45,7 +45,7 @@ class Sexp
     end
     nodes
   end
-  
+
   def subject
     if [:attrasgn, :call, :class, :iter].include? node_type
       self[1]
@@ -57,37 +57,37 @@ class Sexp
       self[1]
     end
   end
-  
+
   def message
     if [:attrasgn, :call, :defs, :iter].include? node_type
       self[2]
     end
   end
-  
+
   def arguments
     if [:attrasgn, :call].include? node_type
       self[3]
     end
   end
-  
+
   def call
     if [:if, :arglist].include? node_type
       self[1]
     end
   end
-  
+
   def conditional_statement
     if node_type == :if
       self[1]
     end
   end
-  
+
   def true_node
     if :if == node_type
       self[2]
     end
   end
-  
+
   def false_node
     if :if == node_type
       self[3]
@@ -99,7 +99,7 @@ class Sexp
       self[1]
     end
   end
-  
+
   def body
     if :block == node_type
       self[1..-1]
@@ -111,7 +111,7 @@ class Sexp
       self[4][1]
     end
   end
-  
+
   def to_s
     if [:lvar, :ivar].include? node_type
       self[1].to_s
@@ -119,6 +119,8 @@ class Sexp
       self[1]
     elsif :lit == node_type
       ":#{self[1]}"
+    elsif :const == node_type
+      self[1]
     elsif :array == node_type
       "[\"#{self.children.collect(&:to_s).join('", "')}\"]"
     elsif :hash == node_type
@@ -132,11 +134,11 @@ class Sexp
       result += "}"
     end
   end
-  
+
   #def to_ruby
     #Ruby2Ruby.new.process(self) unless self.empty?
   #end
-  
+
   #def to_ruby_string
     #return nil if self.empty?
     #eval(Ruby2Ruby.new.process(self)).to_s
