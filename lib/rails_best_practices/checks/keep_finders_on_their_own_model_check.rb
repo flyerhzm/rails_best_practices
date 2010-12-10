dc534f39aea033dbc9d18822d0b653b9cf60fe0f
Richard Huang <flyerhzm@gmail.com>
2010-12-10 23:53:52 +0800

    comments for keep_finders_on_their_own_model

--- lib/rails_best_practices/checks/keep_finders_on_their_own_model_check.rb
+++ lib/rails_best_practices/checks/keep_finders_on_their_own_model_check.rb
@@ -3,28 +3,67 @@ require 'rails_best_practices/checks/check'
 
 module RailsBestPractices
   module Checks
-    # Check a model to make sure finders are on their own model.
+    # Check model files to ake sure finders are on their own model.
     #
-    # Implementation: check if :find is called by other model.
+    # See the best practice details here http://rails-bestpractices.com/posts/13-keep-finders-on-their-own-model.
+    #
+    # Implementation:
+    #
+    # Prepare process:
+    #   none
+    #
+    # Review process:
+    #   check all call nodes in model files.
+    #
+    #   if the call node is a finder (find, all, first or last),
+    #   and the it calls the other model,
+    #   and there are arguments of finder,
+    #   then it should keep finders on its own model.
     class KeepFindersOnTheirOwnModelCheck < Check
-      
-      def interesting_nodes
+
+      def interesting_review_nodes
         [:call]
       end
 
-      def interesting_files
+      def interesting_review_files
         MODLE_FILES
       end
 
-      def evaluate_start(node)
-        add_error "keep finders on their own model" if others_finder?(node)
+      # check all the call nodes to see if there is a finder for other model.
+      #
+      # if the call node is
+      #
+      # 1. the message of call node is one of the :find, :all, :first or :last
+      # 2. the subject of call node is also a call node (it's the other model)
+      # 3. the size of arguments is more than 1 (complex finder)
+      #
+      # then it should keep finders on its own model.
+      def review_start_call(node)
+        add_error "keep finders on their own model" if other_finder?(node)
       end
 
       private
-
-      def others_finder?(node)
-        [:find, :all, :first, :last].include? node.message and node.subject.node_type == :call and node.arguments.size > 1
-      end
+        # check if the call node is the finder of other model.
+        #
+        # the message of the node should be one of :find, :all, :first or :last,
+        # and the subject of the node should be with message :call (this is the other model),
+        # and the arguments of the node should more than 1, like
+        #
+        #     s(:call,
+        #       s(:call, s(:self), :comment, s(:arglist)),
+        #       :find,
+        #       s(:arglist, s(:lit, :all),
+        #         s(:hash,
+        #           s(:lit, :conditions),
+        #           s(:hash, s(:lit, :is_spam), s(:false)),
+        #           s(:lit, :limit),
+        #           s(:lit, 10)
+        #         )
+        #       )
+        #     )
+        def other_finder?(node)
+          [:find, :all, :first, :last].include?(node.message) && :call == node.subject.node_type && node.arguments.size > 1
+        end
     end
   end
 end
