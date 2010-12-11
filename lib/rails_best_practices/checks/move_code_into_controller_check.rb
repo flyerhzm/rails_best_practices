c15f02b969ddd73c773ea4bb5e8d8b2443de7a4a
Richard Huang <flyerhzm@gmail.com>
2010-12-11 14:45:26 +0800

    refactor and comment for move_code_into_controller_check

--- lib/rails_best_practices/checks/move_code_into_controller_check.rb
+++ lib/rails_best_practices/checks/move_code_into_controller_check.rb
@@ -3,30 +3,50 @@ require 'rails_best_practices/checks/check'
 
 module RailsBestPractices
   module Checks
-    # Check a view file to make sure there is no finder.
+    # Check a view file to make sure there is no finder, finder should be moved to controller.
     #
-    # Implementation: Check if view file contains finder, then the code should move to controller.
+    # See the best practice details here http://rails-bestpractices.com/posts/24-move-code-into-controller.
+    #
+    # Implementation:
+    #
+    # Prepare process:
+    #   none
+    #
+    # Review process:
+    #   only check all view files to see if there are finders, then the finders should be moved to controller.
     class MoveCodeIntoControllerCheck < Check
-      
-      FINDER = [:find, :all, :first, :last]
-      
-      def interesting_nodes
+
+      FINDERS = [:find, :all, :first, :last]
+
+      def interesting_review_nodes
         [:call]
       end
-      
-      def interesting_files
+
+      def interesting_review_files
         VIEW_FILES
       end
-      
-      def evaluate_start(node)
+
+      # check call nodes in review process.
+      #
+      # if the subject of the call node is a constant,
+      # and the message of the call node is one of the :find, :all, :first and :last,
+      # then it is a finder and should be moved to controller.
+      def review_start_call(node)
         add_error "move code into controller" if finder?(node)
       end
-      
-      private
 
-      def finder?(node)
-        node.subject.node_type == :const && FINDER.include?(node.message)
-      end
+      private
+        # check if the node is a finder call node.
+        # e.g. the following call node is a finder
+        #
+        #     s(:call,
+        #       s(:const, :Post),
+        #       :find,
+        #       s(:arglist, s(:lit, :all))
+        #     )
+        def finder?(node)
+          :const == node.subject.node_type && FINDERS.include?(node.message)
+        end
     end
   end
 end
