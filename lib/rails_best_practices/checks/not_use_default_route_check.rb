42fed0916474fcb564549ce8c18670c1a58f4bbd
Richard Huang <flyerhzm@gmail.com>
2010-12-12 22:30:32 +0800

    comment for not_use_default_route_check

--- lib/rails_best_practices/checks/not_use_default_route_check.rb
+++ lib/rails_best_practices/checks/not_use_default_route_check.rb
@@ -3,23 +3,54 @@ require 'rails_best_practices/checks/check'
 
 module RailsBestPractices
   module Checks
-    # Check config/routes to make sure not use default route that rails generated.
+    # Check config/routes file to make sure not use default route that rails generated.
     #
-    # Implementation: compare route sentence to see if it is equal to rails default route.
+    # See the best practice details here http://rails-bestpractices.com/posts/12-not-use-default-route-if-you-use-restful-design
+    #
+    # Implementation:
+    #
+    # Prepare process:
+    #   none
+    #
+    # Review process:
+    #   check all method call to see if any method call is the same as rails default route.
+    #
+    #     map.connect ':controller/:action/:id'
+    #     map.connect ':controller/:action/:id.:format'
+    #
+    #   or
+    #
+    #     match ':controller(/:action(/:id(.:format)))'
     class NotUseDefaultRouteCheck < Check
-      
-      def interesting_nodes
+
+      def interesting_review_nodes
         [:call]
       end
-      
-      def interesting_files
+
+      def interesting_review_files
         /config\/routes.rb/
       end
-      
-      def evaluate_start(node)
-        if node == s(:call, s(:lvar, :map), :connect, s(:arglist, s(:str, ":controller/:action/:id"))) or
-           node == s(:call, s(:lvar, :map), :connect, s(:arglist, s(:str, ":controller/:action/:id.:format"))) or
-           node == s(:call, nil, :match, s(:arglist, s(:str, ":controller(/:action(/:id(.:format)))")))
+
+      # check all method calls, it just compare with rails default route
+      #
+      # rails2
+      #
+      #     s(:call, s(:lvar, :map), :connect,
+      #       s(:arglist, s(:str, ":controller/:action/:id"))
+      #     )
+      #     s(:call, s(:lvar, :map), :connect,
+      #       s(:arglist, s(:str, ":controller/:action/:id.:format"))
+      #     )
+      #
+      # rails3
+      #
+      #     s(:call, nil, :match,
+      #       s(:arglist, s(:str, ":controller(/:action(/:id(.:format)))"))
+      #     )
+      def review_start_call(node)
+        if s(:call, s(:lvar, :map), :connect, s(:arglist, s(:str, ":controller/:action/:id"))) == node ||
+           s(:call, s(:lvar, :map), :connect, s(:arglist, s(:str, ":controller/:action/:id.:format"))) == node ||
+           s(:call, nil, :match, s(:arglist, s(:str, ":controller(/:action(/:id(.:format)))"))) == node
           add_error "not use default route"
         end
       end
