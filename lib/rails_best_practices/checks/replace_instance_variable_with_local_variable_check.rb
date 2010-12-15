ba80fa52e04f331f395acdfb21a60a6ec70a8aca
Richard Huang <flyerhzm@gmail.com>
2010-12-15 20:42:15 +0800

    comments for replace_instance_variable_with_local_variable_check

--- lib/rails_best_practices/checks/replace_instance_variable_with_local_variable_check.rb
+++ lib/rails_best_practices/checks/replace_instance_variable_with_local_variable_check.rb
@@ -5,18 +5,29 @@ module RailsBestPractices
   module Checks
     # Check a partail view file to make sure there is no instance variable.
     #
-    # Implementation: Check all instance variable, if exists, then it should be replaced with local variable
+    # See the best practice details here http://rails-bestpractices.com/posts/27-replace-instance-variable-with-local-variable.
+    #
+    # Implementation:
+    #
+    # Prepare process:
+    #   none
+    #
+    # Review process:
+    #   check all instance variable in partial view files,
+    #   if exist, then they should be replaced with local variable
     class ReplaceInstanceVariableWithLocalVariableCheck < Check
-      
-      def interesting_nodes
+
+      def interesting_review_nodes
         [:ivar]
       end
-      
-      def interesting_files
+
+      def interesting_review_files
         PARTIAL_VIEW_FILES
       end
-      
-      def evaluate_start(node)
+
+      # check ivar node in partial view file,
+      # it is an instance variable, and should be replaced with local variable.
+      def review_start_ivar(node)
         add_error "replace instance variable with local variable"
       end
     end
