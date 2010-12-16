12399c9f31898e3da80ccaf8cd625ec9e76b7734
Richard Huang <flyerhzm@gmail.com>
2010-12-16 22:04:55 +0800

    refactor and comments for use_observer_check

--- lib/rails_best_practices/checks/use_observer_check.rb
+++ lib/rails_best_practices/checks/use_observer_check.rb
@@ -3,51 +3,153 @@ require 'rails_best_practices/checks/check'
 
 module RailsBestPractices
   module Checks
-    # Check a model file to make sure mail deliver method is in observer not callback.
+    # Make sure to use observer (sorry we only check the mailer deliver now).
     #
-    # Implementation: 
-    # Record :after_create callback
-    # Check method define, if it is a callback and call deliver_xxx message in method body, then it should use observer.
+    # See the best practice details here http://rails-bestpractices.com/posts/19-use-observer.
+    #
+    # TODO: we need a better solution, any suggestion?
+    #
+    # Implementation:
+    #
+    # Prepare process:
+    #   check all class nodes to see if they are the subclass of ActionMailer::Base,
+    #   if so, remember the class name.
+    #
+    # Review process:
+    #   check all call nodes to see if they are callback definitions, like after_create, before_destroy,
+    #   if so, remember the callback methods.
+    #
+    #   check all method define nodes to see
+    #   if the method is a callback method,
+    #   and there is a mailer deliver call,
+    #   then the method should be replaced by using observer.
     class UseObserverCheck < Check
 
-      def interesting_nodes
+      def interesting_prepare_nodes
+        [:class]
+      end
+
+      def interesting_review_nodes
         [:defn, :call]
       end
 
-      def interesting_files
-        MODLE_FILES
+      def interesting_prepare_files
+        MAILER_FILES
+      end
+
+      def interesting_review_files
+        MODEL_FILES
       end
 
       def initialize
         super
         @callbacks = []
+        @mailer_names = []
       end
 
-      def evaluate_start(node)
-        if :after_create == node.message
-          remember_callbacks(node)
-        elsif :defn == node.node_type and @callbacks.find { |callback| equal?(callback, node.message_name) }
-          add_error "use observer" if use_observer?(node)
+      # check class node in prepare process.
+      #
+      # if it is a subclass of ActionMailer::Base,
+      # then remember its class name.
+      def prepare_start_class(node)
+        remember_mailer_names(node)
+      end
+
+      # check a call node in review process.
+      #
+      # if it is a callback definition, like
+      #
+      #     after_create :send_create_notification
+      #     before_destroy :send_destroy_notification
+      #
+      # then remember its callback methods (:send_create_notification).
+      def review_start_call(node)
+        remember_callback(node)
+      end
+
+      # check a method define node in prepare process.
+      #
+      # if it is callback method,
+      # and there is a actionmailer deliver call in the method define node,
+      # then it should be replaced by using observer.
+      def review_start_defn(node)
+        if callback_method?(node) and deliver_mailer?(node)
+          add_error "use observer"
         end
       end
 
       private
-
-      def remember_callbacks(node)
-        node.arguments[1..-1].each do |argument|
-          # ignore callback like after_create Comment.new
-          if :lit == argument.node_type
-            @callbacks << argument.to_s
+        # check a class node, if its base class is ActionMailer::Base, like
+        #
+        #     s(:class, :ProjectMailer,
+        #       s(:colon2, s(:const, :ActionMailer), :Base),
+        #       s(:scope)
+        #     )
+        #
+        # then save the class name in @mailer_names
+        def remember_mailer_names(node)
+          if s(:colon2, s(:const, :ActionMailer), :Base) == node.base_class
+            @mailer_names << node.class_name
           end
         end
-      end
 
-      def use_observer?(node)
-        node.recursive_children do |child|
-          return true if :call == child.node_type and :const == child.subject.node_type and child.message.to_s =~ /^deliver_/
+        # check a call node, if it is a callback definition, such as after_create, before_create, like
+        #
+        #     s(:call, nil, :after_create,
+        #       s(:arglist, s(:lit, :send_create_notification))
+        #     )
+        #
+        # then save the callback methods in @callbacks
+        #
+        #     @callbacks => [:send_create_notification]
+        def remember_callback(node)
+          if node.message.to_s =~ /^after_|^before_/
+            node.arguments[1..-1].each do |argument|
+              # ignore callback like after_create Comment.new
+              @callbacks << argument.to_s if :lit == argument.node_type
+            end
+          end
         end
-        false
-      end
+
+        # check a defn node to see if the method name exists in the @callbacks.
+        def callback_method?(node)
+          @callbacks.find { |callback| equal?(callback, node.method_name) }
+        end
+
+        # check a defn node to see if it contains a actionmailer deliver call.
+        #
+        # for rails2
+        #
+        # if the message of call node is deliver_xxx,
+        # and the subject of the call node exists in @callbacks, like
+        #
+        #     s(:call, s(:const, :ProjectMailer), :deliver_notification,
+        #       s(:arglist, s(:self), s(:lvar, :member))
+        #     )
+        #
+        # for rails3
+        #
+        # if the message of call node is deliver,
+        # and the subject of the call node is with subject node who exists in @callbacks, like
+        #
+        #     s(:call,
+        #       s(:call, s(:const, :ProjectMailer), :notification,
+        #         s(:arglist, s(:self), s(:lvar, :member))
+        #       ),
+        #       :deliver,
+        #       s(:arglist)
+        #     )
+        #
+        # then the call node is actionmailer deliver call.
+        def deliver_mailer?(node)
+          node.grep_nodes(:node_type => :call).each do |child_node|
+            # rails2 actionmailer deliver
+            return true if child_node.message.to_s =~ /^deliver_/ && @mailer_names.include?(child_node.subject.to_s)
+            # rails3 actionmailer deliver
+            return true if :deliver == child_node.message && @mailer_names.include?(child_node.subject.subject.to_s)
+          end
+          false
+        end
     end
   end
 end
--- spec/rails_best_practices/checks/use_observer_check_spec.rb
+++ spec/rails_best_practices/checks/use_observer_check_spec.rb
@@ -3,74 +3,153 @@ require 'spec_helper'
 describe RailsBestPractices::Checks::UseObserverCheck do
   before(:each) do
     @runner = RailsBestPractices::Core::Runner.new(RailsBestPractices::Checks::UseObserverCheck.new)
+
+    content =<<-EOF
+    class ProjectMailer < ActionMailer::Base
+    end
+    EOF
+    @runner.prepare('app/models/project_mailer.rb', content)
   end
 
-  it "should use observer" do
-    content =<<-EOF
-    class Project < ActiveRecord::Base
-      after_create :send_create_notification
+  describe "rails2" do
+    it "should use observer" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create :send_create_notification
 
-      private
+        private
 
-      def send_create_notification
-        self.members.each do |member|
-          ProjectMailer.deliver_notification(self, member)
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.deliver_notification(self, member)
+          end
         end
       end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/models/project.rb:6 - use observer"
     end
-    EOF
-    @runner.check('app/models/project.rb', content)
-    errors = @runner.errors
-    errors.should_not be_empty
-    errors[0].to_s.should == "app/models/project.rb:6 - use observer"
-  end
 
-  it "should not use observer without callback" do
-    content =<<-EOF
-    class Project < ActiveRecord::Base
-      private
+    it "should not use observer without callback" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        private
 
-      def send_create_notification
-        self.members.each do |member|
-          ProjectMailer.deliver_notification(self, member)
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.deliver_notification(self, member)
+          end
         end
       end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should be_empty
     end
-    EOF
-    @runner.check('app/models/project.rb', content)
-    errors = @runner.errors
-    errors.should be_empty
-  end
 
-  it "should use observer with two after_create" do
-    content =<<-EOF
-    class Project < ActiveRecord::Base
-      after_create :send_create_notification, :update_author
+    it "should use observer with two after_create" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create :send_create_notification, :update_author
 
-      private
+        private
 
-      def send_create_notification
-        self.members.each do |member|
-          ProjectMailer.deliver_notification(self, member)
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.deliver_notification(self, member)
+          end
+        end
+
+        def update_author
         end
       end
-
-      def update_author
-      end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/models/project.rb:6 - use observer"
     end
-    EOF
-    @runner.check('app/models/project.rb', content)
-    errors = @runner.errors
-    errors.should_not be_empty
-    errors[0].to_s.should == "app/models/project.rb:6 - use observer"
+
+    it "should not raise when initiate an object in callback" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create ProjectMailer.new
+      end
+      EOF
+      lambda { @runner.check('app/models/project.rb', content) }.should_not raise_error
+    end
   end
 
-  it "should not raise when initiate an object in callback" do
-    content =<<-EOF
-    class Project < ActiveRecord::Base
-      after_create ProjectMailer.new
+  describe "rails3" do
+    it "should use observer" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create :send_create_notification
+
+        private
+
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.notification(self, member).deliver
+          end
+        end
+      end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/models/project.rb:6 - use observer"
     end
-    EOF
-    lambda { @runner.check('app/models/project.rb', content) }.should_not raise_error
+
+    it "should not use observer without callback" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        private
+
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.notification(self, member).deliver
+          end
+        end
+      end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should be_empty
+    end
+
+    it "should use observer with two after_create" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create :send_create_notification, :update_author
+
+        private
+
+        def send_create_notification
+          self.members.each do |member|
+            ProjectMailer.notification(self, member).deliver
+          end
+        end
+
+        def update_author
+        end
+      end
+      EOF
+      @runner.check('app/models/project.rb', content)
+      errors = @runner.errors
+      errors.should_not be_empty
+      errors[0].to_s.should == "app/models/project.rb:6 - use observer"
+    end
+
+    it "should not raise when initiate an object in callback" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        after_create ProjectMailer.new
+      end
+      EOF
+      lambda { @runner.check('app/models/project.rb', content) }.should_not raise_error
+    end
   end
 end
