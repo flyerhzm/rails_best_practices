8f574c85af687b1ba7bac02abae3da603d086aff
Richard Huang <flyerhzm@gmail.com>
2010-12-16 23:36:59 +0800

    comments for use_say_with_time_in_migrations_check

--- spec/rails_best_practices/checks/use_say_with_time_in_migrations_check_spec.rb
+++ spec/rails_best_practices/checks/use_say_with_time_in_migrations_check_spec.rb
@@ -21,6 +21,27 @@ describe RailsBestPractices::Checks::UseSayWithTimeInMigrationsCheck do
     errors[0].to_s.should == "db/migrate/20101010080658_update_users.rb:2 - use say with time in migrations"
   end
 
+  it "should use say with time in migrations with create_table" do
+    content =<<-EOF
+    def self.up
+      create_table :users do |t|
+        t.string :login
+        t.timestamps
+      end
+
+      User.find_each do |user|
+        user.first_name, user.last_name = user.full_name.split(' ')
+        user.save
+      end
+    end
+    EOF
+    @runner.check('db/migrate/20101010080658_update_users.rb', content)
+    errors = @runner.errors
+    errors.should_not be_empty
+    errors.size.should == 1
+    errors[0].to_s.should == "db/migrate/20101010080658_update_users.rb:7 - use say with time in migrations"
+  end
+
   it "should not use say with time in migrations" do
     content =<<-EOF
     def self.up
@@ -57,17 +78,17 @@ describe RailsBestPractices::Checks::UseSayWithTimeInMigrationsCheck do
     content =<<-EOF
     class AddAdmin < ActiveRecord::Migration
 
-      class Person < ActiveRecord::Base  
+      class Person < ActiveRecord::Base
       end
-      
+
       class Blog < ActiveRecord::Base
       end
 
       def self.up
         add_column :people, :admin, :boolean, :default => false, :null => false
-        add_column :people, :deactivated, :boolean, 
+        add_column :people, :deactivated, :boolean,
                             :default => false, :null => false
-        
+
         key = Crypto::Key.from_file("\#{RAILS_ROOT}/rsa_key.pub")
         person = Person.new(:email => "admin@example.com",
                             :name => "admin",
