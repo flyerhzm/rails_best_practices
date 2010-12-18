dc7bc23aae86b250c4a46d9417340f1c34dc7279
Richard Huang <flyerhzm@gmail.com>
2010-12-19 01:03:50 +0800

    comment for core_ext.rb

--- lib/rails_best_practices/core/core_ext.rb
+++ lib/rails_best_practices/core/core_ext.rb
@@ -1,11 +1,13 @@
 # encoding: utf-8
 module Enumerable
+  # return the duplicated entries.
   def dups
     inject({}) {|h,v| h[v]=h[v].to_i+1; h}.reject{|k,v| v==1}.keys
   end
 end
 
 class NilClass
+  # do not stop when causing messages on nil object.
   def method_missing(method_sym, *arguments, &block)
     return nil
   end
