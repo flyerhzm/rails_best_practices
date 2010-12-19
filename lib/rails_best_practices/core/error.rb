394bfe2306af50e78e2fefc2c779c06a9353e8b5
Richard Huang <flyerhzm@gmail.com>
2010-12-19 18:49:43 +0800

    comments for error.rb

--- lib/rails_best_practices/core/error.rb
+++ lib/rails_best_practices/core/error.rb
@@ -1,15 +1,18 @@
 # encoding: utf-8
 module RailsBestPractices
   module Core
+    # Error is the violation to rails best practice.
+    #
+    # it indicts the filenname, line number and error message for the violation.
     class Error
       attr_reader :filename, :line_number, :message
-      
+
       def initialize(filename, line_number, message)
         @filename = filename
         @line_number = line_number
         @message = message
       end
-      
+
       def to_s
         "#{@filename}:#{@line_number} - #{@message}"
       end
