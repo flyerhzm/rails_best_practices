f2dd6a1a2bd64d86b9639f629abbf0a4cf021298
Richard Huang <flyerhzm@gmail.com>
2010-12-19 18:23:16 +0800

    meta programming for checking_visitor initialize and better comment for review and prepare method

--- lib/rails_best_practices/core/checking_visitor.rb
+++ lib/rails_best_practices/core/checking_visitor.rb
@@ -16,18 +16,14 @@ module RailsBestPractices
     class CheckingVisitor
       # remember all the checks for prepare and review processes according to interesting_prepare_nodes and interesting_review_nodes,
       def initialize(checks)
-        @prepare_checks ||= {}
-        @review_checks ||= {}
-        checks.each do |check|
-          (check.interesting_prepare_nodes || []).each do |node|
-            @prepare_checks[node] ||= []
-            @prepare_checks[node] << check
-            @prepare_checks[node].uniq!
-          end
-          (check.interesting_review_nodes || []).each do |node|
-            @review_checks[node] ||= []
-            @review_checks[node] << check
-            @review_checks[node].uniq!
+        [:prepare, :review].each do |process|
+          instance_variable_set("@#{process}_checks", {})                  # @review_checks = {}
+          checks.each do |check|                                           # checks.each do |check|
+            check.send("interesting_#{process}_nodes").each do |node|      # check.interesting_review_nodes.each do |node|
+              instance_variable_get("@#{process}_checks")[node] ||= []     #   @review_checks[node] ||= []
+              instance_variable_get("@#{process}_checks")[node] << check   #   @review_checks[node] << check
+              instance_variable_get("@#{process}_checks")[node].uniq!      #   @review_checks[node].uniq!
+            end                                                            # end
           end
         end
       end
@@ -41,12 +37,24 @@ module RailsBestPractices
       # then run the reivew for that node.
       [:prepare, :review].each do |process|
         class_eval <<-EOS
-          def #{process}(node)
-            checks = @#{process}_checks[node.node_type]
-            checks.each {|check| check.#{process}_node_start(node) if node.file =~ check.interesting_#{process}_files} unless checks.nil?
-            node.children.each {|sexp| sexp.#{process}(self)}
-            checks.each {|check| check.#{process}_node_end(node) if node.file =~ check.interesting_#{process}_files} unless checks.nil?
-          end
+          def #{process}(node)                                         # def review(node)
+            checks = @#{process}_checks[node.node_type]                #   checks = @review_checks[node.node_type]
+            if checks                                                  #   if checks
+              checks.each { |check|                                    #     checks.each { |check|
+                if node.file =~ check.interesting_#{process}_files     #      if node.file =~ check.interesting_review_files
+                  check.#{process}_node_start(node)                    #         check.review_node_start(node)
+                end                                                    #       end
+              }                                                        #     }
+            end                                                        #   end
+            node.children.each {|sexp| sexp.#{process}(self)}          #   node.children.each {|sexp| sexp.review(self)}
+            if checks                                                  #   if checks
+              checks.each { |check|                                    #     checks.each { |check|
+                if node.file =~ check.interesting_#{process}_files     #       if node.file =~ check.interesting_review_files
+                  check.#{process}_node_end(node)                      #         check.review_node_end(node)
+                end                                                    #       end
+              }                                                        #     }
+            end                                                        #   end
+          end                                                          # end
         EOS
       end
     end
