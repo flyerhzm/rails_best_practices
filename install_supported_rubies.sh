9ea4fd5dd791e318f6bcb2f173fe444d6d2b0c35
Richard Huang <flyerhzm@gmail.com>
2011-01-26 23:24:02 +0800

    update rvm script for test

--- install_supported_rubies.sh
+++ install_supported_rubies.sh
@@ -2,7 +2,6 @@
 rubies=( 1.8.7 1.9.2 ree )
 for x in ${rubies[*]}
 do
-  rvm install $x || exit $?
-  rvm $x && rvm gemset use global && gem install bundler || exit $?
-  bundle install || exit $?
+  rvm install $x
+  rvm $x && rvm gemset use global && gem install bundler
 done
