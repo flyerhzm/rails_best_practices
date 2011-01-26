aa2f81e9370a20eb872d46bd1f52051c96061140
Richard Huang <flyerhzm@gmail.com>
2011-01-26 23:24:02 +0800

    update rvm script for test

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -4,6 +4,7 @@ gemset="rails_best_practices"
 
 for x in ${rubies[*]}
 do
-  rvm use --create $x@$gemset && bundle install || exit $?
+  rvm use --create $x@$gemset
+  bundle install
+  rake spec:progress
 done
-rvm ree@$gemset,1.8.7@$gemset,1.9.2@$gemset rake spec:progress
