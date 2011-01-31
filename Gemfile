b7a4926c0d05d275a400abab791cdaca341c842e
Richard Huang <flyerhzm@gmail.com>
2011-01-31 23:46:37 +0800

    Bumping rspec to 2.4.0

--- Gemfile
+++ Gemfile
@@ -1,3 +1,2 @@
 source "http://rubygems.org"
-gem 'rake'
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
+require 'rspec/expectations'
 require 'rails_best_practices'
