9eb22a4b99c9b2fba2edd47891325477f8ab60d7
Richard Huang <flyerhzm@gmail.com>
2011-03-16 23:47:04 +0800

    take care of :class_name option for model associations

--- spec/rails_best_practices/prepares/model_prepare_spec.rb
+++ spec/rails_best_practices/prepares/model_prepare_spec.rb
@@ -14,9 +14,55 @@ describe RailsBestPractices::Prepares::ModelPrepare do
     EOF
     runner.prepare('app/models/project.rb', content)
     model_associations = RailsBestPractices::Prepares.model_associations
-    model_associations["Project"]["portfolio"].should == :belongs_to
-    model_associations["Project"]["project_manager"].should == :has_one
-    model_associations["Project"]["milestones"].should == :has_many
-    model_associations["Project"]["categories"].should == :has_and_belongs_to_many
+    model_associations["Project"]["portfolio"].should == {:belongs_to => "Portfolio"}
+    model_associations["Project"]["project_manager"].should == {:has_one => "ProjectManager"}
+    model_associations["Project"]["milestones"].should == {:has_many => "Milestone"}
+    model_associations["Project"]["categories"].should == {:has_and_belongs_to_many => "Category"}
+  end
+
+  context "class_name" do
+    it "should parse belongs_to" do
+      content =<<-EOF
+      class Post < ActiveRecord::Base
+        belongs_to :author, :class_name => "Person"
+      end
+      EOF
+      runner.prepare("app/models/post.rb", content)
+      model_associations = RailsBestPractices::Prepares.model_associations
+      model_associations["Post"]["author"].should == {:belongs_to => "Person"}
+    end
+
+    it "should parse has_one" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        has_one :project_manager, :class_name => "Person"
+      end
+      EOF
+      runner.prepare("app/models/post.rb", content)
+      model_associations = RailsBestPractices::Prepares.model_associations
+      model_associations["Project"]["project_manager"].should == {:has_one => "Person"}
+    end
+
+    it "should parse has_many" do
+      content =<<-EOF
+      class Project < ActiveRecord::Base
+        has_many :people, :class_name => "Person"
+      end
+      EOF
+      runner.prepare("app/models/project.rb", content)
+      model_associations = RailsBestPractices::Prepares.model_associations
+      model_associations["Project"]["people"].should == {:has_many => "Person"}
+    end
+
+    it "should parse has_and_belongs_to_many" do
+      content =<<-EOF
+      class Citizen < ActiveRecord::Base
+        has_and_belongs_to_many :nations, :class_name => "Country"
+      end
+      EOF
+      runner.prepare("app/models/citizen.rb", content)
+      model_associations = RailsBestPractices::Prepares.model_associations
+      model_associations["Citizen"]["nations"].should == {:has_and_belongs_to_many => "Country"}
+    end
   end
 end
