f5601aca19ed7029137a3e8b2114c3a9ab747bf0
Richard Huang <flyerhzm@gmail.com>
2011-10-05 14:51:23 +0800

    controller_prepare supports inherited_resources

--- lib/rails_best_practices/prepares/controller_prepare.rb
+++ lib/rails_best_practices/prepares/controller_prepare.rb
@@ -5,8 +5,10 @@ module RailsBestPractices
   module Prepares
     # Remember controllers and controller methods
     class ControllerPrepare < Core::Check
+      DEFAULT_ACTIONS = %w(index show new create edit update destroy)
+
       def interesting_nodes
-        [:class, :def]
+        [:class, :def, :command, :var_ref]
       end
 
       def interesting_files
@@ -15,11 +17,41 @@ module RailsBestPractices
 
       def initialize
         @methods = Prepares.controller_methods
+        @inherited_resources = false
       end
 
-      # check class node to remember the last class name.
+      # check class node to remember the class name.
+      # also check if the controller is inherit from InheritedResources::Base.
       def start_class(node)
-        @last_klazz = node.class_name.to_s
+        @class_name = node.class_name.to_s
+        if "InheritedResources::Base" == node.base_class.to_s
+          @inherited_resources = true
+          @actions = DEFAULT_ACTIONS
+        end
+      end
+
+      # remember the action names at the end of class node if the controller is a InheritedResources.
+      def end_class(node)
+        if @inherited_resources
+          @actions.each do |action|
+            @methods.add_method(@class_name, action)
+          end
+        end
+      end
+
+      # check if there is a DSL call inherit_resources.
+      def start_var_ref(node)
+        if "inherit_resources" == node.to_s
+          @inherited_resources = true
+          @actions = DEFAULT_ACTIONS
+        end
+      end
+
+      # restrict actions for inherited_resources
+      def start_command(node)
+        if @inherited_resources && "actions" ==  node.message.to_s
+          @actions = node.arguments.all.map(&:to_s)
+        end
       end
 
       # check def node to remember all methods.
@@ -31,7 +63,7 @@ module RailsBestPractices
       #     }
       def start_def(node)
         method_name = node.method_name.to_s
-        @methods.add_method(@last_klazz, method_name)
+        @methods.add_method(@class_name, method_name)
       end
     end
   end
--- spec/rails_best_practices/prepares/controller_prepare_spec.rb
+++ spec/rails_best_practices/prepares/controller_prepare_spec.rb
@@ -21,4 +21,38 @@ describe RailsBestPractices::Prepares::ControllerPrepare do
     methods = RailsBestPractices::Prepares.controller_methods
     methods.get_methods("PostsController").should == ["index", "show"]
   end
+
+  describe "inherited_resources" do
+    it "extend inherited_resources" do
+      content =<<-EOF
+      class PostsController < InheritedResources::Base
+      end
+      EOF
+      runner.prepare('app/controllers/posts_controller.rb', content)
+      methods = RailsBestPractices::Prepares.controller_methods
+      methods.get_methods("PostsController").should == ["index", "show", "new", "create", "edit", "update", "destroy"]
+    end
+
+    it "extend inherited_resources with actions" do
+      content =<<-EOF
+      class PostsController < InheritedResources::Base
+        actions :index, :show
+      end
+      EOF
+      runner.prepare('app/controllers/posts_controller.rb', content)
+      methods = RailsBestPractices::Prepares.controller_methods
+      methods.get_methods("PostsController").should == ["index", "show"]
+    end
+
+    it "DSL inherit_resources" do
+      content =<<-EOF
+      class PostsController
+        inherit_resources
+      end
+      EOF
+      runner.prepare('app/controllers/posts_controller.rb', content)
+      methods = RailsBestPractices::Prepares.controller_methods
+      methods.get_methods("PostsController").should == ["index", "show", "new", "create", "edit", "update", "destroy"]
+    end
+  end
 end
