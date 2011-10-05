65959452b029c1076fc9700d30016701674ce7af
Richard Huang <flyerhzm@gmail.com>
2011-10-05 13:41:28 +0800

    add restrict auto-generated routes check

--- lib/rails_best_practices/core.rb
+++ lib/rails_best_practices/core.rb
@@ -8,6 +8,7 @@ require 'rails_best_practices/core/models'
 require 'rails_best_practices/core/model_associations'
 require 'rails_best_practices/core/model_attributes'
 require 'rails_best_practices/core/mailers'
+require 'rails_best_practices/core/methods'
 
 require 'rails_best_practices/core_ext/sexp'
 require 'rails_best_practices/core_ext/enumerable'
