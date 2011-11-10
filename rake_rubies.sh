71a794d239c9f408eea5060a3701ae074fc9c382
Richard Huang <flyerhzm@gmail.com>
2011-11-10 17:22:20 +0800

    only test 1.9.2-p180, 1.9.2-p290 and 1.9.3-p0

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-rubies=( 1.8.7 1.9.2 ree )
+rubies=( 1.9.2-p180 1.9.2-p290 1.9.3-p0 )
 gemset="rails_best_practices"
 
 for x in ${rubies[*]}
 do
-  rvm use --create $x@$gemset
-  bundle install
-  rake spec:progress
+  echo $x@$gemset
+  rvm $x@$gemset do bundle exec rake spec:progress
 done
