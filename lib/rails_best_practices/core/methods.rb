0685a72851e5c72495531ea14fe73f7fee4edf19
Richard Huang <flyerhzm@gmail.com>
2011-11-19 19:04:47 +0800

    do not add method if it is already existed

--- lib/rails_best_practices/core/methods.rb
+++ lib/rails_best_practices/core/methods.rb
@@ -16,6 +16,7 @@ module RailsBestPractices
       # @param [String] access control, public, protected or private
       def add_method(class_name, method_name, meta={}, access_control="public")
         return if class_name == ""
+        return if has_method?(class_name, method_name)
         methods(class_name) << Method.new(class_name, method_name, access_control, meta)
         if access_control == "public"
           @possible_methods[method_name] = false
