b5b7c5b56d5daea69beca59ea8410f675d87ed5e
Richard Huang <flyerhzm@gmail.com>
2011-11-19 19:10:32 +0800

    abstract Callable from remove_unused_methods_in_models_review

--- lib/rails_best_practices/reviews/remove_unused_methods_in_models_review.rb
+++ lib/rails_best_practices/reviews/remove_unused_methods_in_models_review.rb
@@ -14,8 +14,8 @@ module RailsBestPractices
     class RemoveUnusedMethodsInModelsReview < Review
       include Klassable
       include Completeable
+      include Callable
 
-      interesting_nodes :call, :fcall, :var_ref, :command_call, :command, :alias, :bare_assoc_hash, :method_add_arg
       interesting_files ALL_FILES
 
       EXCEPT_METHODS = %w(initialize validate to_xml to_json assign_attributes after_find after_initialize)
@@ -24,75 +24,6 @@ module RailsBestPractices
         super()
         @model_methods = Prepares.model_methods
         @except_methods = EXCEPT_METHODS + options['except_methods']
-      end
-
-      # remember the message of call node.
-      def start_call(node)
-        mark_used(node.message)
-      end
-
-      # remember the message of fcall node.
-      def start_fcall(node)
-        mark_used(node.message)
-      end
-
-      # remember name of var_ref node.
-      def start_var_ref(node)
-        mark_used(node)
-      end
-
-      # remember the message of command call node.
-      def start_command_call(node)
-        mark_used(node.message)
-      end
-
-      # remember the message of command node.
-      # remember the argument of alias_method and alias_method_chain as well.
-      def start_command(node)
-        case node.message.to_s
-        when "named_scope", "scope"
-          # nothing
-        when "alias_method"
-          mark_used(node.arguments.all[1])
-        when "alias_method_chain"
-          method, feature = *node.arguments.all.map(&:to_s)
-          call_method("#{method}_with_#{feature}")
-        else
-          mark_used(node.message)
-          node.arguments.all.each { |argument| mark_used(argument) }
-        end
-      end
-
-      # remember the old method of alias node.
-      def start_alias(node)
-        mark_used(node.old_method)
-      end
-
-      # remember hash values for hash key "methods".
-      #
-      #     def to_xml(options = {})
-      #       super options.merge(:exclude => :visible, :methods => [:is_discussion_conversation])
-      #     end
-      def start_bare_assoc_hash(node)
-        if node.hash_keys.include? "methods"
-          mark_used(node.hash_value("methods"))
-        end
-      end
-
-      # remember the first argument for try and send method.
-      def start_method_add_arg(node)
-        case node.message.to_s
-        when "try"
-          method_name = node.arguments.all[0].to_s
-          call_method(method_name)
-        when "send"
-          if [:symbol_literal, :string_literal].include?(node.arguments.all[0].sexp_type)
-            method_name = node.arguments.all[0].to_s
-            call_method(method_name)
-          end
-        else
-          # nothing
-        end
       end
 
       # get all unused methods at the end of review process.
@@ -104,26 +35,9 @@ module RailsBestPractices
         end
       end
 
-      private
-        def mark_used(method_node)
-          if :bare_assoc_hash == method_node.sexp_type
-            method_node.hash_values.each { |value_node| mark_used(value_node) }
-          elsif :array == method_node.sexp_type
-            method_node.array_values.each { |value_node| mark_used(value_node) }
-          else
-            method_name = method_node.to_s
-          end
-          call_method(method_name)
-        end
-
-        def call_method(method_name)
-          if @model_methods.has_method?(current_class_name, method_name)
-            @model_methods.get_method(current_class_name, method_name).mark_used
-          end
-          @model_methods.mark_parent_class_method_used(current_class_name, method_name)
-          @model_methods.mark_subclasses_method_used(current_class_name, method_name)
-          @model_methods.possible_public_used(method_name)
-        end
+      def methods
+        @model_methods
+      end
     end
   end
 end
