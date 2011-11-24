a0a755eeb1da9b224fa9c2b79fc6593978385f12
Richard Huang <flyerhzm@gmail.com>
2011-11-24 21:12:55 +0800

    parse controller name with namespace

--- spec/rails_best_practices/core/routes_spec.rb
+++ spec/rails_best_practices/core/routes_spec.rb
@@ -7,4 +7,16 @@ describe RailsBestPractices::Core::Routes do
     routes.add_route(["admin", "test"], "posts", "new")
     routes.map(&:to_s).should == ["Admin::Test::PostsController#new"]
   end
+
+  context "route" do
+    it "should add namesapces, controller name and action name" do
+      route = RailsBestPractices::Core::Route.new(['admin', 'test'], 'posts', 'new')
+      route.to_s.should == "Admin::Test::PostsController#new"
+    end
+
+    it "should add controller name with namespace" do
+      route = RailsBestPractices::Core::Route.new(['admin'], 'test/posts', 'new')
+      route.to_s.should == "Admin::Test::PostsController#new"
+    end
+  end
 end
