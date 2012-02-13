0581a73a82e4ec3c523deeb7d38993b6353e6d78
Richard Huang <flyerhzm@gmail.com>
2012-02-12 23:08:06 -0800

    add not_use_time_ago_in_words_review

---
+++ spec/rails_best_practices/reviews/not_use_times_ago_in_words_review_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe RailsBestPractices::Reviews::NotUseTimeAgoInWordsReview do
+  let(:runner) { RailsBestPractices::Core::Runner.new(:reviews => RailsBestPractices::Reviews::NotUseTimeAgoInWordsReview.new) }
+
+  describe "time_ago_in_words" do
+    it "should not use in views" do
+      content =<<-EOF
+      <%= time_ago_in_words(post.created_at) %>
+      EOF
+      runner.review('app/views/posts/show.html.erb', content)
+      runner.should have(1).errors
+      runner.errors[0].to_s.should == "app/views/posts/show.html.erb:1 - not use time_ago_in_words"
+    end
+
+    it "should not use in helpers" do
+      content =<<-EOF
+      def timeago
+        content_tag(:p, time_ago_in_words(post.created_at))
+      end
+      EOF
+      runner.review('app/helpers/posts_helper.rb', content)
+      runner.should have(1).errors
+      runner.errors[0].to_s.should == "app/helpers/posts_helper.rb:2 - not use time_ago_in_words"
+    end
+  end
+
+  describe "distance_of_time_in_words_to_now" do
+    it "should not use in views" do
+      content =<<-EOF
+      <%= distance_of_time_in_words_to_now(post.created_at) %>
+      EOF
+      runner.review('app/views/posts/show.html.erb', content)
+      runner.should have(1).errors
+      runner.errors[0].to_s.should == "app/views/posts/show.html.erb:1 - not use time_ago_in_words"
+    end
+
+    it "should not use in helpers" do
+      content =<<-EOF
+      def timeago
+        content_tag(:p, distance_of_time_in_words_to_now(post.created_at))
+      end
+      EOF
+      runner.review('app/helpers/posts_helper.rb', content)
+      runner.should have(1).errors
+      runner.errors[0].to_s.should == "app/helpers/posts_helper.rb:2 - not use time_ago_in_words"
+    end
+  end
+end
