415168dec7f364155f6b67d5c921464cdc6b4258
Richard Huang <flyerhzm@gmail.com>
2012-02-12 23:08:06 -0800

    add not_use_time_ago_in_words_review

---
+++ lib/rails_best_practices/reviews/not_use_time_ago_in_words_review.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+require 'rails_best_practices/reviews/review'
+
+module RailsBestPractices
+  module Reviews
+    # Review view and helper files to make sure not use time_ago_in_words or distance_of_time_in_words_to_now.
+    #
+    # See the best practice details here http://rails-bestpractices.com/posts/105-not-use-time_ago_in_words.
+    #
+    # Implementation:
+    #
+    # Review process:
+    #   check all fcall node to see if its message is time_ago_in_words and distance_of_time_in_words_to_now
+    class NotUseTimeAgoInWordsReview < Review
+      interesting_nodes :fcall
+      interesting_files VIEW_FILES, HELPER_FILES
+
+      def url
+        "http://rails-bestpractices.com/posts/105-not-use-time_ago_in_words"
+      end
+
+      # check fcall node to see if its message is time_ago_in_words or distance_of_time_in_words_to_now
+      def start_fcall(node)
+        if "time_ago_in_words" == node.message.to_s || "distance_of_time_in_words_to_now" == node.message.to_s
+          add_error "not use time_ago_in_words"
+        end
+      end
+    end
+  end
+end
