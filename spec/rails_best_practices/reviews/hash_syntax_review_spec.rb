38525f83fab428af316263efedab664d17fd2e9b
Dominik Masur <dominik.masur@googlemail.com>
2012-06-08 11:47:51 +0200

    Added Option to warn about StringHashes

--- spec/rails_best_practices/reviews/hash_syntax_review_spec.rb
+++ spec/rails_best_practices/reviews/hash_syntax_review_spec.rb
@@ -15,18 +15,30 @@ module RailsBestPractices
         runner.should have(1).errors
         runner.errors[0].to_s.should == "app/models/user.rb:2 - change Hash Syntax to 1.9"
       end
-
-      it "should find 1.8 Hash with string" do
-        content =<<-EOF
-        class User < ActiveRecord::Base
-          CONST = { "foo" => "bar" }
+      context "should find 1.8 Hash with string" do
+        it "and report them with Report StringHashes Option" do
+          runner = Core::Runner.new(reviews: HashSyntaxReview.new(report_string_hash: true))
+          content =<<-EOF
+          class User < ActiveRecord::Base
+            CONST = { "foo" => "bar" }
+          end
+          EOF
+          runner.review('app/models/user.rb', content)
+          runner.should have(1).errors
+          runner.errors[0].to_s.should == "app/models/user.rb:2 - change Hash Syntax to 1.9"
         end
-        EOF
-        runner.review('app/models/user.rb', content)
-        runner.should have(1).errors
-        runner.errors[0].to_s.should == "app/models/user.rb:2 - change Hash Syntax to 1.9"
-      end
 
+        it "and not report them without Report StringHashes Option" do
+          runner = Core::Runner.new(reviews: HashSyntaxReview.new(report_string_hash: false))
+          content =<<-EOF
+          class User < ActiveRecord::Base
+            CONST = { "foo" => "bar" }
+          end
+          EOF
+          runner.review('app/models/user.rb', content)
+          runner.should have(0).errors
+        end
+      end
       it "should not alert on 1.9 Syntax" do
         content =<<-EOF
         class User < ActiveRecord::Base
