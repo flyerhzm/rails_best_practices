6f6cf5ee79eb95a96f575ab68746281a32ab493c
Richard Huang <flyerhzm@gmail.com>
2012-06-08 08:41:30 +0800

    add comments to hash_syntax_review

--- lib/rails_best_practices/reviews/hash_syntax_review.rb
+++ lib/rails_best_practices/reviews/hash_syntax_review.rb
@@ -3,10 +3,17 @@ require 'rails_best_practices/reviews/review'
 
 module RailsBestPractices
   module Reviews
+    # Check ruby 1.8 style hash and suggest to change hash syntax to 1.9.
+    #
+    # Review process:
+    #   check hash nodes in all files,
+    #   if the sexp type of hash key nodes is not :@lable,
+    #   then the hash is ruby 1.8 style.
     class HashSyntaxReview < Review
       interesting_nodes :hash, :bare_assoc_hash
       interesting_files ALL_FILES
 
+      # check hash node to see if it is ruby 1.8 style.
       def start_hash(node)
         pair_nodes = node[1][1]
 
@@ -15,6 +22,7 @@ module RailsBestPractices
         end
       end
 
+      # check bare_assoc_hash node to see if it is ruby 1.8 style.
       def start_bare_assoc_hash(node)
         pair_nodes = node[1]
 
@@ -24,6 +32,10 @@ module RailsBestPractices
       end
 
       protected
+        # check if hash key/value pairs are ruby 1.8 style.
+        #
+        # hash key of ruby 1.9 style is :@label,
+        # so if it is not, then it is ruby 1.8 style.
         def hash_is_18?(pair_nodes)
           return false if pair_nodes.blank?
 
