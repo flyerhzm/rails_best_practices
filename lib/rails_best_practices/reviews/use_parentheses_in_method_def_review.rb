7607401f2956481196f76375b9750c029cf557d7
Richard Huang <flyerhzm@gmail.com>
2012-06-19 08:06:01 +0800

    add comments to use_parenthese_in_method_def_review

--- lib/rails_best_practices/reviews/use_parentheses_in_method_def_review.rb
+++ lib/rails_best_practices/reviews/use_parentheses_in_method_def_review.rb
@@ -3,25 +3,30 @@ require 'rails_best_practices/reviews/review'
 
 module RailsBestPractices
   module Reviews
+    # Check if method definition has parentheses around parameters.
+    #
+    # Review process:
+    #   check def node in all files,
+    #   if params node with values, but not wrapped by paren node,
+    #   then it should use parentheses around parameters.
     class UseParenthesesInMethodDefReview < Review
       interesting_nodes :def
       interesting_files ALL_FILES
 
+      # check def node to see if parameters are wrapped by parentheses.
       def start_def(node)
-        if no_parentheses_around_parameters? node
-          if method_has_parameters? node
-            add_error("use parentheses around parameters in method definitions")
-          end
+        if no_parentheses_around_parameters?(node) && has_parameters?(node)
+          add_error("use parentheses around parameters in method definitions")
         end
       end
 
       protected
-        def no_parentheses_around_parameters?(node)
-          not node[2][0] == :paren
+        def no_parentheses_around_parameters?(def_node)
+          :parent != def_node[2][0]
         end
 
-        def method_has_parameters?(node)
-          not node[2][1..-1].to_a.compact.empty?
+        def has_parameters?(def_node)
+          :params == def_node[2][0] && !def_node[2][1..-1].compact.empty?
         end
     end
   end
