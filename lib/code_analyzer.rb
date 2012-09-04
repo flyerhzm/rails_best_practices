279d84012ca8209c59daeb1af4ab6f0dc416763f
Richard Huang <flyerhzm@gmail.com>
2012-09-04 11:53:24 +0800

    move interesting_nodes and interesting_files to CodeAnalyzer

--- lib/code_analyzer.rb
+++ lib/code_analyzer.rb
@@ -2,6 +2,7 @@ require 'code_analyzer/nil'
 require 'code_analyzer/sexp'
 
 module CodeAnalyzer
-   autoload :CheckingVisitor, "code_analyzer/checking_visitor"
    autoload :AnalyzerException, "code_analyzer/analyzer_exception"
+   autoload :Checker, "code_analyzer/checker"
+   autoload :CheckingVisitor, "code_analyzer/checking_visitor"
 end
