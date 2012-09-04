9eeff970a75e0764b3af33503fff5cf05904f329
Richard Huang <flyerhzm@gmail.com>
2012-09-04 11:15:28 +0800

    move sexp and nil to code_analyzer

--- lib/code_analyzer.rb
+++ lib/code_analyzer.rb
@@ -1,3 +1,6 @@
+require 'code_analyzer/nil'
+require 'code_analyzer/sexp'
+
 module CodeAnalyzer
    autoload :CheckingVisitor, "code_analyzer/checking_visitor"
    autoload :AnalyzerException, "code_analyzer/analyzer_exception"
