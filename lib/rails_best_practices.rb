76f2123291b896757c9ceb71b15bac1522c62dd1
Richard Huang <flyerhzm@gmail.com>
2012-09-12 16:31:59 +0800

    fix typo

--- lib/rails_best_practices.rb
+++ lib/rails_best_practices.rb
@@ -8,7 +8,7 @@
 # "Software"), to deal in the Software without restriction, including
 # without limitation the rights to use, copy, modify, merge, publish,
 # distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, receiver to
+# permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
 #
 # The above copyright notice and this permission notice shall be
