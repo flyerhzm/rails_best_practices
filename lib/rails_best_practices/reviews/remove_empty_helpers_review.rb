31336c91bd7423cd6007261a76ddcfea45e88dd4
Richard Huang <flyerhzm@gmail.com>
2012-10-07 18:06:04 +0800

    don't remove empty application_helper.rb

--- lib/rails_best_practices/reviews/remove_empty_helpers_review.rb
+++ lib/rails_best_practices/reviews/remove_empty_helpers_review.rb
@@ -17,11 +17,16 @@ module RailsBestPractices
       url "http://rails-bestpractices.com/posts/72-remove-empty-helpers"
 
       # check the body of module node, if it is nil, then it should be removed.
-      add_callback :start_module do |node|
-        if s(:bodystmt, s(:stmts_add, s(:stmts_new), s(:void_stmt)), nil, nil, nil) == node.body
-          add_error "remove empty helpers", node.file, node.line
+      add_callback :start_module do |module_node|
+        if "ApplicationHelper" != module_node.module_name.to_s && empty_body?(module_node)
+          add_error "remove empty helpers"
         end
       end
+
+      protected
+        def empty_body?(module_node)
+          s(:bodystmt, s(:stmts_add, s(:stmts_new), s(:void_stmt)), nil, nil, nil) == module_node.body
+        end
     end
   end
 end
