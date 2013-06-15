5452d9ac20e14c24022f64d219725e851923ee83
Richard Huang <flyerhzm@gmail.com>
2013-06-15 18:46:46 +0800

    hotfix for ARGV.first is nil #169

--- lib/rails_best_practices/command.rb
+++ lib/rails_best_practices/command.rb
@@ -122,7 +122,7 @@ OptionParser.new do |opts|
   opts.parse!
 end
 
-unless File.exists?(ARGV.first)
+if ARGV.size > 0 && !File.exists?(ARGV.first)
   puts "#{ARGV.first} doesn't exist"
   exit 1
 end
