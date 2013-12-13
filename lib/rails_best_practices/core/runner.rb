6b7eacdf49b7c7a063cab3dad7302596c4fde676
Ostrzy <uslugi@ostrzy.net>
2013-12-13 01:04:49 +0100

    Ensure that options is Hash and we can check for empty?

--- lib/rails_best_practices/core/runner.rb
+++ lib/rails_best_practices/core/runner.rb
@@ -132,6 +132,7 @@ module RailsBestPractices
             begin
               check_name, options = *check
               klass = RailsBestPractices::Lexicals.const_get(check_name)
+              options = Hash(options)
               active_checks << (options.empty? ? klass.new : klass.new(options))
             rescue
               # the check does not exist in the Lexicals namepace.
@@ -151,6 +152,7 @@ module RailsBestPractices
             begin
               check_name, options = *check
               klass = RailsBestPractices::Reviews.const_get(check_name.gsub(/Check$/, 'Review'))
+              options = Hash(options)
               active_checks << (options.empty? ? klass.new : klass.new(options))
             rescue
               # the check does not exist in the Reviews namepace.
